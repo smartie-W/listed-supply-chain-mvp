fb8418b7f7c8b82c0779982ea6f541e4e1d7c274
王白白 <wang@wangbaibaideMacBook-Air.local>
2026-02-23 23:12:16 +0800

    chore: switch default api base to serveo backup tunnel

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Frontend API endpoint.
 // NOTE: this is a temporary tunnel endpoint.
-window.APP_API_BASE = 'https://2590e66b70be37.lhr.life';
+window.APP_API_BASE = 'https://732118d01853ce99-116-235-145-49.serveousercontent.com';
